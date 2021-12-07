202a3206115294f2b8920ee2dad8c6506ba09f74
danial2026 <1998alirezam@gmail.com>
2021-12-07 11:22:29 +0330

    delete some files

--- internal/profile/controllers/users_controller_dto.go
+++
@@ -1,8 +0,0 @@
-package controllers
-
-type response struct {
-	Timestamp string `json:"timestamp"`
-	Status    int    `json:"status"`
-	Message   string `json:"message"`
-	Path      string `json:"path"`
-}
